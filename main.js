0b7b1d8004f18f19b57e1d743da74faf11d7faac
CraigYeoman <yeomancraig@gmail.com>
2022-01-16 11:30:43 -0600

    Added changing player/computer score/choice

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        let playerSelection = button.id;
+        let playerSelection = button.id;        
         console.log(playerSelection);
         let computerSelection = computerPlay();
         console.log(computerSelection);
+        playerPicture(playerSelection);
+        computerPicture(computerSelection);
         playRound(playerSelection, computerSelection);
         console.log('Player score =', playerScore);
         console.log('Computer score =', computerScore);
-
+        playerScoreBoard(playerScore);
+        computerScoreBoard(computerScore);
         if (playerScore == 5) {
             console.log('Player wins the game!');
         } else if (computerScore == 5) {
@@ -24,10 +27,77 @@ buttons.forEach((button) => {
 function computerPlay() {
     const computerOptions = ['rock', 'paper', 'scissors']
     let computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    return computerSelection
+    
+    return computerSelection;
 }
 
+function computerPicture(computerSelection) {
+    let computerSign = '';
+    if (computerSelection === 'rock') {
+        computerSign = '🪨'
+    } else if (computerSelection === 'paper') {
+        computerSign = '🧾'
+    } else if (computerSelection === 'scissors') {
+        computerSign = '✂️'
+    }
+    const computerRemove = document.getElementById("computer-Remove");
+    computerRemove.remove();
+    
+    let computerPick = document.querySelector('#computer');
+    let picks = document.createElement('div');
+    picks.classList.add('sign');
+    picks.id = 'computer-Remove';
+    picks.textContent = computerSign;
+    computerPick.appendChild(picks);
+}
 
+function playerPicture(playerSelection) {
+    let playerSign = '';
+    if (playerSelection === 'rock') {
+        playerSign = '🪨'
+    } else if (playerSelection === 'paper') {
+        playerSign = '🧾'
+    } else if (playerSelection === 'scissors') {
+        playerSign = '✂️'
+    }
+
+    const playerRemove = document.getElementById("player-Remove");
+    playerRemove.remove();
+    
+    let playerPick = document.querySelector('#player');
+    let picks = document.createElement('div');
+    picks.classList.add('sign');
+    picks.id = 'player-Remove';
+    picks.textContent = playerSign;
+    playerPick.appendChild(picks);
+}
+
+function playerScoreBoard(playerScore) {
+
+
+    const playerScoreRemove = document.getElementById("player-Score");
+    playerScoreRemove.remove();
+    
+    let playerPoints = document.querySelector('#player-Board');
+    let points = document.createElement('div');
+    points.classList.add('sign');
+    points.id = 'player-Score';
+    points.textContent = playerScore;
+    playerPoints.appendChild(points);
+}
+
+function computerScoreBoard(computerScore) {
+
+    const computerScoreRemove = document.getElementById("computer-Score");
+    computerScoreRemove.remove();
+    
+    let computerPoints = document.querySelector('#computer-Board');
+    let points = document.createElement('div');
+    points.classList.add('sign');
+    points.id = 'computer-Score';
+    points.textContent = computerScore;
+    computerPoints.appendChild(points);
+}
 
 function playRound(playerSelection, computerSelection) {
      if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
