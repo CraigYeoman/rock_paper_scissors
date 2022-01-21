51fa0b5c93c23272b30ff4c4b924673f35c773c7
CraigYeoman <yeomancraig@gmail.com>
2022-01-21 06:47:19 -0600

    fixing corrupted files

--- main.js
+++ main.js
@@ -1,59 +1,167 @@
 let playerScore = 0
 let computerScore = 0
+let gameWinner = ''
+
+
+const playerOptions = document.getElementById('player-options');
+const buttons = playerOptions.querySelectorAll('button');
+const restartButton = document.getElementById('restart-button');
+// getElementById
+
+const playerPick = document.getElementById('player');
+const computerPick = document.getElementById('computer');
+const modal = document.getElementById("endGameModal");
+const modalText = document.getElementById("endGameMessage");
+
+restartButton.addEventListener('click', restartGame)
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.id;        
+        let computerSelection = computerPlay();
+        playerPicture(playerSelection);
+        computerPicture(computerSelection);
+        playRound(playerSelection, computerSelection);
+        console.log('Player score =', playerScore);
+        console.log('Computer score =', computerScore);
+        playerScoreBoard(playerScore);
+        computerScoreBoard(computerScore);
+
+        if (playerScore == 5) {
+            console.log('Player wins the game!');
+            removeInfo();
+            newInfo.textContent = 'Player wins the game!';
+            infoTitle.appendChild(newInfo);
+            gameWinner = 'Player'
+            modalTransition();
+        } else if (computerScore == 5) {
+            console.log('Computer wins the game!');
+            removeInfo();
+            newInfo.textContent = 'Computer wins the game!';
+            infoTitle.appendChild(newInfo);
+            gameWinner = 'Computer'
+            modalTransition();
+        }
+    });
+});
 
 function computerPlay() {
     const computerOptions = ['rock', 'paper', 'scissors']
     let computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    return computerSelection
+    return computerSelection;
 }
 
+function computerPicture(computerSelection) {
+    let computerSign = '';
+    if (computerSelection === 'rock') {
+        computerSign = '🪨'
+    } else if (computerSelection === 'paper') {
+        computerSign = '🧾'
+    } else if (computerSelection === 'scissors') {
+        computerSign = '✂️'
+    }
+    const computerRemove = document.getElementById("computer-Remove");
+    computerRemove.remove();
+    
+    
+    let picks = document.createElement('div');
+    picks.classList.add('sign');
+    picks.id = 'computer-Remove';
+    picks.textContent = computerSign;
+    computerPick.appendChild(picks);
+}
+
+function playerPicture(playerSelection) {
+    let playerSign = '';
+    if (playerSelection === 'rock') {
+        playerSign = '🪨'
+    } else if (playerSelection === 'paper') {
+        playerSign = '🧾'
+    } else if (playerSelection === 'scissors') {
+        playerSign = '✂️'
+    }
+
+    const playerRemove = document.getElementById("player-Remove");
+    playerRemove.remove();
+    
+    
+    let picks = document.createElement('div');
+    picks.classList.add('sign');
+    picks.id = 'player-Remove';
+    picks.textContent = playerSign;
+    playerPick.appendChild(picks);
+}
+
+function playerScoreBoard(playerScore) {
+    const playerScoreRemove = document.getElementById("player-Score");
+    playerScoreRemove.remove();
+    
+    let playerPoints = document.querySelector('#player-Board');
+    let points = document.createElement('div');
+    points.classList.add('sign');
+    points.id = 'player-Score';
+    points.textContent = playerScore;
+    playerPoints.appendChild(points);
+}
+
+function computerScoreBoard(computerScore) {
+    const computerScoreRemove = document.getElementById("computer-Score");
+    computerScoreRemove.remove();
+    
+    let computerPoints = document.querySelector('#computer-Board');
+    let points = document.createElement('div');
+    points.classList.add('sign');
+    points.id = 'computer-Score';
+    points.textContent = computerScore;
+    computerPoints.appendChild(points);
+}
+
+const infoTitle = document.querySelector('#info-title');
+const newInfo = document.createElement('div');
+function removeInfo(){
+    const informationRemove = document.getElementById("info-replace");
+    informationRemove.remove();
+    newInfo.classList.add('sign');
+    newInfo.id = 'info-replace';
+}
 
 
 function playRound(playerSelection, computerSelection) {
-     if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
-         (playerSelection === 'paper' && computerSelection === 'rock') || 
-         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-         playerScore++    
-         console.log('You win the round!')
-     }
-
-     else if (playerSelection == computerSelection) {
-        console.log('Tie!')
-     }
-
-     else {((playerSelection == 'rock' && computerSelection == 'paper') 
-     || (playerSelection == 'paper' && computerSelection == 'scissors') 
-     || (playerSelection == 'scissors' && computerSelection == 'rock')) 
-        computerScore++
-        console.log('You lose the round:(')
-     }
-     
+    
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'paper' && computerSelection === 'rock') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        playerScore++;    
+        console.log('You win the round!');
+        removeInfo();
+        newInfo.textContent = 'You win the round!';
+        infoTitle.appendChild(newInfo);
+    } else if (playerSelection == computerSelection) {
+        console.log('Tie!');
+        removeInfo();
+        newInfo.textContent = 'Tie!';
+        infoTitle.appendChild(newInfo);
+    } else {((playerSelection == 'rock' && computerSelection == 'paper') || 
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock')) 
+        computerScore++;
+        removeInfo();
+        console.log('You lose the round:(');
+        newInfo.textContent = 'You lose the round!';
+        infoTitle.appendChild(newInfo);
+    }   
 }
 
-console.log(game())
+function modalTransition() {
+    endGameModal.classList.add('activate');
+    modalText.textContent = gameWinner, 'wins the game';
+}
 
-
-
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        let round = i;
-        console.log('Round', round);
-        let playerSelection = window.prompt('Rock, paper, or scissors?', '');
-        console.log('Player choice =',playerSelection);
-        let computerSelection = computerPlay();
-        console.log('Computer choice =', computerSelection);
-        computerSelection;
-        playerSelection;
-        playRound(playerSelection, computerSelection);
-        console.log('Player score =', playerScore);
-        console.log('Computer score =', computerScore);
-    }
-    if (playerScore > computerScore) {
-        console.log('Player wins the game!');
-    } else if (playerScore == computerScore) {
-        console.log('Game ends in a tie!');
-    } else {
-        console.log('Computer wins game!');
-    }
-    return 'Refresh page to play again'
+function restartGame() {
+    playerScore = 0
+    computerScore = 0
+    endGameModal.classList.remove('activate');
+    computerRemove.remove();
+    playerRemove.remove();
+    gameWinner = ''
 }
